62ffed96df743821e36375700e138a11eb8378a3
Chad Bowler <chadbowler@gmail.com>
2023-08-16 12:41:25 -0600

    added a search algorithm to get possibilities for each square

--- sudoku-solver.py
+++ sudoku-solver.py
@@ -15,33 +15,88 @@ game_square_dict = {}
 
 
 class Game_square:
-    def __init__(self, x, y, value):
+    def __init__(self, x, y, big_square, value):
         self.x = x
         self.y = y
         self.value = value
+        self.big_square = big_square
+        self.check_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
     def __repr__(self):
         return f"{self.value}"
     
+    def print_big_squares(self):
+        for x in game_square_dict.items():
+            print(x.self.big_square)
+    
     def test_possibilities(self):
         if self.value == 0:
-            pattern = '\(0, \d\)'
+            pattern = f'\({self.x}, \d\)'
             for key, value in game_square_dict.items():
                 if re.match(pattern, str(key)):
-                    print(key, value)
+                    # print(key, value)
+                    
+                    for i, x in enumerate(self.check_array):
+                        
+                        if str(self.check_array[i]) == str(value):
+                            self.check_array.remove(self.check_array[i])
+                            print(self.check_array)
+
+            pattern = f'\(\d, {self.y}\)'
+            for key, value in game_square_dict.items():
+                if re.match(pattern, str(key)):
+                    # print(key, value)
+                    
+                    for i, x in enumerate(self.check_array):
+                        if str(self.check_array[i]) == str(value):
+                            self.check_array.remove(self.check_array[i])
+                            print(self.check_array)
+        # self.print_big_squares()                    
+    
+
+def print_all():
+    for key, value in enumerate(game_square_dict): 
+        print(f'square: {key}, fff {value}')    
 
 def check_possibilites():
-      game_square_dict[0, 1].test_possibilities()
+    #   this = game_square_dict[0, 1]
+    #   print(this)
+    #   this.test_possibilities()
+      for key, value in enumerate(game_square_dict):
+        
+        game_square_dict[value].test_possibilities()
+        # print(f'square: {keys}, fff {value}') 
+
+
+      
 
 def initialize_classes():
     
     for i, value in enumerate(puzzle_array):
         for j, value in enumerate(puzzle_array[i]):
-            game_square_dict[i, j] = Game_square(i, j, value)
+            if i < 3 and j < 3:
+                big_square = 1
+            elif i < 3 and 2 < j < 6:
+                big_square = 2
+            elif i < 3 and 5 < j:
+                big_square = 3
+            elif 2 < i < 6 and j < 3:
+                big_square = 4
+            elif 2 < i < 6 and 2 < j < 6:
+                big_square = 5
+            elif 2 < i < 6 and 5 < j:
+                big_square = 6
+            elif i > 5 and j < 3:
+                big_square = 7
+            elif i > 5 and 2 < j < 6:
+                big_square = 8
+            elif i > 5 and 5 < j:
+                big_square = 9
+            game_square_dict[i, j] = Game_square(i, j, big_square, value)
 
-    # print(game_square_dict)
     check_possibilites()
-            
+    # print_all()
+
 
 
 if __name__=='__main__':
